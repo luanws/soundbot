4b4c72a41f5e5b81893877f1a16e8d1ac9f0d930
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-02-04 15:03:55 -0300

    projector

--- desktop/src/ui/projector/__init__.py
+++ desktop/src/ui/projector/__init__.py
@@ -1,41 +1,38 @@
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtWidgets import QMainWindow
-from src.ui.projector.view_model import ProjectorViewModel
+
+from src.settings.projector_font_settings import ProjectorFontSettings
 from src.ui.projector.window import Ui_MainWindow
 
 
 class ProjectorWindow(QMainWindow, Ui_MainWindow):
+    font_settings: ProjectorFontSettings
+    __text: str
+
     def __init__(self, parent=None):
         super().__init__(parent)
         super().setupUi(self)
 
+        self.font_settings = ProjectorFontSettings()
+        self.__text = ''
+
         self.setWindowIcon(QtGui.QIcon('icon.ico'))
 
-        self.__view_model = ProjectorViewModel()
-        self.__view_model.on_change_text(self.on_change_text)
-        self.__view_model.font_settings.add_settings_listener(self.configure_text_label_styles)
+        self.font_settings.add_settings_listener(self.configure_text_label_styles)
 
         self.text_label.setText('')
         self.configure_text_label_styles()
 
     @property
     def text(self):
-        return self.__view_model.text
+        return self.__text
 
     @text.setter
     def text(self, text: str):
-        self.__view_model.text = text
-
-    def keyPressEvent(self, event: QKeyEvent):
-        key = event.key()
-        if key == QtCore.Qt.Key_Escape:
-            self.close()
+        self.__text = text
+        self.on_change_text(text)
 
     def close(self) -> bool:
-        if self.text_label.text():
-            self.text_label.setText('')
-            return False
         return super().close()
 
     def show(self) -> None:
@@ -46,7 +43,7 @@ class ProjectorWindow(QMainWindow, Ui_MainWindow):
         self.text_label.setText(text)
 
     def configure_text_label_styles(self):
-        font_settings = self.__view_model.font_settings
+        font_settings = self.font_settings
         self.text_label.setAlignment(QtCore.Qt.AlignVCenter)
         self.text_label.setStyleSheet(f"""
             color: {font_settings.color};
