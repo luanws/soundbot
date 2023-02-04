f3008df75fa6bce69fb5b05c1b9bba6353458616
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-02-04 16:33:01 -0300

    save video path

--- desktop/src/ui/video_path_settings/__init__.py
+++ desktop/src/ui/video_path_settings/__init__.py
@@ -18,12 +18,27 @@ class VideoPathSettingsWindow(QMainWindow, Ui_MainWindow):
 
         self.setWindowIcon(QtGui.QIcon('icon.ico'))
 
-        self.hymns_widget = VideoPathWidget(title='Hinos', path=self.video_path_settings.hymns)
-        self.songs_widget = VideoPathWidget(title='Músicas', path=self.video_path_settings.songs)
-        self.taste_and_see_widget = VideoPathWidget(title='Provai e vede', path=self.video_path_settings.taste_and_see)
-        self.offertory_widget = VideoPathWidget(title='Ofertório', path=self.video_path_settings.offertory)
+        hymns_path = self.video_path_settings.hymns
+        songs_path = self.video_path_settings.songs
+        taste_and_see_path = self.video_path_settings.taste_and_see
+        offertory_path = self.video_path_settings.offertory
+
+        self.hymns_widget = VideoPathWidget(title='Hinos', path=hymns_path)
+        self.songs_widget = VideoPathWidget(title='Músicas', path=songs_path)
+        self.taste_and_see_widget = VideoPathWidget(title='Provai e vede', path=taste_and_see_path)
+        self.offertory_widget = VideoPathWidget(title='Ofertório', path=offertory_path, is_file=True)
 
         self.vertical_layout.addWidget(self.hymns_widget)
         self.vertical_layout.addWidget(self.songs_widget)
         self.vertical_layout.addWidget(self.taste_and_see_widget)
         self.vertical_layout.addWidget(self.offertory_widget)
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        self.video_path_settings.hymns = self.hymns_widget.path
+        self.video_path_settings.songs = self.songs_widget.path
+        self.video_path_settings.taste_and_see = self.taste_and_see_widget.path
+        self.video_path_settings.offertory = self.offertory_widget.path
+
+        self.video_path_settings.save()
+
+        event.accept()
--- desktop/src/ui/video_path_settings/widgets/video_path.py
+++ desktop/src/ui/video_path_settings/widgets/video_path.py
@@ -5,8 +5,10 @@ from src.widgets.icon_button_widget import IconButton
 
 
 class VideoPathWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None, *, title: str, path: str):
+    def __init__(self, parent=None, *, title: str, path: str, is_file: bool = None):
         super().__init__(parent)
+
+        self.is_file = is_file
 
         color = styles.qss_vars['@colorIcon']
         active_color = styles.qss_vars['@colorActiveIcon']
@@ -27,6 +29,8 @@ class VideoPathWidget(QtWidgets.QWidget):
         """)
 
         self.label.setText(title)
+        self.path_line_edit.setText(path)
+        self.select_path_button.clicked.connect(self.select_path)
 
         self.edit_container.addWidget(self.path_line_edit)
         self.edit_container.addWidget(self.select_path_button)
@@ -35,3 +39,14 @@ class VideoPathWidget(QtWidgets.QWidget):
         self.container.addLayout(self.edit_container)
 
         self.setLayout(self.container)
+
+    @property
+    def path(self) -> str:
+        return self.path_line_edit.text()
+
+    def select_path(self):
+        if self.is_file:
+            path, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Selecionar arquivo', self.path_line_edit.text())
+        else:
+            path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Selecionar pasta', self.path_line_edit.text())
+        self.path_line_edit.setText(path)
