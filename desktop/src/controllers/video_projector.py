2ee47e04fe28521c3c7dca5f2371e0d7f37854e7
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-02-04 23:11:17 -0300

    Update video_projector.py

--- desktop/src/controllers/video_projector.py
+++ desktop/src/controllers/video_projector.py
@@ -1,4 +1,4 @@
-
+import os
 from typing import TYPE_CHECKING, Any, Dict
 
 from src.utils.settings.video_path_settings import VideoPathSettings
@@ -14,20 +14,14 @@ class VideoProjectorController:
         self.video_projector_window = video_projector_window
         self.video_path_settings = VideoPathSettings()
 
-    def make_video_path(self, video_type: str, filename: str) -> str:
-        path = self.video_path_settings.get_path_from_video_type(video_type)
-        if video_type == 'offertory':
-            return path
-        return f'{path}/{filename}'
-
     def play_video(self, data: Dict[str, Any]) -> str:
-        video_type = data['type']
-        filename = data.get('filename', '')
-        video_path = self.make_video_path(video_type, filename)
+        dirname = data['dirname']
+        filename = data['filename']
+        video_path = os.path.join(dirname, filename)
         print(f'Playing video: {video_path}')
         self.video_projector_window.show_in_last_screen()
         self.video_projector_window.play_video(video_path)
-        return filename
+        return video_path
 
     def close_video_projector(self, data: Dict[str, Any]):
         self.video_projector_window.close()
