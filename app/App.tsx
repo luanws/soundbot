3a96f01831bf001e940c4315ebf9c532d1fdf267
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-03-06 20:10:30 -0300

    app: expo splash screen

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
 import { BackgroundColor } from "@bacons/expo-background-color"
 import { NavigationContainer } from "@react-navigation/native"
+import * as NavigationBar from 'expo-navigation-bar'
+import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from "react"
 import { SafeAreaView } from 'react-native'
 import * as Migrations from "./src/database/migrations"
 import { DatabaseUtils } from "./src/database/utils"
 import AppProvider from "./src/hooks"
+import { useAuth } from "./src/hooks/auth"
+import { useSettings } from "./src/hooks/settings"
 import { useTheme } from "./src/hooks/theme"
 import Routes from "./src/routes"
 import './src/utils/prototype/string'
@@ -18,16 +22,32 @@ async function runOnStartup() {
 
 function Wrapper() {
   const theme = useTheme()
+  const { authLoaded } = useAuth()
+  const { isThemeLoading } = useSettings()
 
-  useEffect(() => { runOnStartup() }, [])
+  const loaded = !isThemeLoading && authLoaded
+
+  useEffect(() => {
+    runOnStartup()
+    NavigationBar.setBackgroundColorAsync(theme.colors.navigationBar.background)
+    NavigationBar.setBorderColorAsync(theme.colors.navigationBar.border)
+  }, [])
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
 
   return (
     <>
       <BackgroundColor color={theme.colors.background} />
-      <StatusBar
-        backgroundColor={theme.colors.statusBar.background}
-        style={theme.colors.statusBar.icons}
-      />
+      {loaded && (
+        <StatusBar
+          backgroundColor={theme.colors.statusBar.background}
+          style={theme.colors.statusBar.icons}
+        />
+      )}
       <NavigationContainer>
         <Routes />
       </NavigationContainer>
@@ -36,6 +56,10 @@ function Wrapper() {
 }
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync()
+  }, [])
+
   return (
     <>
       <SafeAreaView style={{ flex: 1 }}>
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,23 +1,11 @@
-import * as NavigationBar from 'expo-navigation-bar'
-import React, { useEffect } from 'react'
+
+import React from 'react'
 import { useAuth } from '../hooks/auth'
-import { useSettings } from '../hooks/settings'
-import { useTheme } from '../hooks/theme'
 import AppRoutes from './app.routes'
 import AuthRoutes from './auth.routes'
 
 const Routes: React.FC = () => {
-  const { authLoaded, authenticated } = useAuth()
-  const { colors } = useTheme()
-  const { isThemeLoading } = useSettings()
-
-  useEffect(() => {
-    NavigationBar.setBackgroundColorAsync(colors.navigationBar.background)
-    NavigationBar.setBorderColorAsync(colors.navigationBar.border)
-  }, [])
-
-  const loaded = authLoaded && !isThemeLoading
-  if (!loaded) return null
+  const { authenticated } = useAuth()
   return authenticated ? <AppRoutes /> : <AuthRoutes />
 }
 
