f008c8efefaf54db79a924a3d38e7c1a494b9c69
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-03-11 16:29:12 -0300

    app: PlayerModal em provai e vede

--- app/src/screens/TasteAndSeeScreen/index.tsx
+++ app/src/screens/TasteAndSeeScreen/index.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import VideoCell from '../../components/Cell/VideoCell'
 import Loading from '../../components/Loading'
+import PlayerModal, { PlayerModalRef } from '../../components/Modal/PlayerModal'
 import SearchFileTree from '../../components/Search/SearchFileTree'
 import { FileTree } from '../../models/file-tree'
+import { CommandService } from '../../services/command'
 import { TasteAndSeeService } from '../../services/taste-and-see'
 import { Divider, TodayTasteAndSeeContainer, TodayTasteAndSeeInfoText, TodayTasteAndSeeTitleText } from './styles'
 
 const TasteAndSeeScreen: React.FC = () => {
+  const playerModalRef = useRef<PlayerModalRef>(null)
+
   const [tasteAndSeeFileTree, setTasteAndSeeFileTree] = useState<FileTree | null>(null)
   const [todayTasteAndSee, setTodayTasteAndSee] = useState<FileTree | null>(null)
+  const [todayTasteAndSeeIsPlaying, setTodayTasteAndSeeIsPlaying] = useState<boolean>(false)
 
   const todayString = new Date().toLocaleDateString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
@@ -31,6 +36,21 @@ const TasteAndSeeScreen: React.FC = () => {
     return TasteAndSeeService.searchTasteAndSees(selectedDirectory.children, searchText)
   }
 
+  async function handleStopTodayTasteAndSee() {
+    await CommandService.stopVideo()
+    setTodayTasteAndSeeIsPlaying(false)
+    playerModalRef.current?.close()
+  }
+
+  async function handlePlayTodayTasteAndSee(todayTasteAndSee: FileTree) {
+    await CommandService.playVideo({
+      dirname: todayTasteAndSee.dirname,
+      filename: todayTasteAndSee.name,
+    })
+    setTodayTasteAndSeeIsPlaying(true)
+    playerModalRef.current?.open()
+  }
+
   return (
     <>
       {tasteAndSeeFileTree ? (
@@ -49,6 +69,7 @@ const TasteAndSeeScreen: React.FC = () => {
                 <VideoCell
                   filename={todayTasteAndSee.name}
                   playing={false}
+                  onPress={() => handlePlayTodayTasteAndSee(todayTasteAndSee)}
                 />
               </TodayTasteAndSeeContainer>
               <Divider />
@@ -56,6 +77,13 @@ const TasteAndSeeScreen: React.FC = () => {
           )}
         />
       ) : <Loading />}
+      {todayTasteAndSee && todayTasteAndSeeIsPlaying && (
+        <PlayerModal
+          ref={playerModalRef}
+          playingVideoFilename={todayTasteAndSee.name}
+          stopVideo={handleStopTodayTasteAndSee}
+        />
+      )}
     </>
   )
 }
