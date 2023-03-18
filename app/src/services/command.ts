c27b992eb4ab6b9ccbdf6d42cfa48af1bc3f4673
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-03-18 13:50:02 -0300

    app: command service html

--- app/src/services/command.ts
+++ app/src/services/command.ts
@@ -16,4 +16,12 @@ export namespace CommandService {
     export async function stopVideo(): Promise<void> {
         await api.post('/command/stop_video')
     }
+
+    export async function showHTML(html: string): Promise<void> {
+        await api.post('/command/show_html', { html })
+    }
+
+    export async function hideHTML(): Promise<void> {
+        await api.post('/command/hide_html')
+    }
 }
