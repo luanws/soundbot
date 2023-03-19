eece314da90ce9c2360e5153bb1754a43828522b
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-03-19 02:40:44 -0300

    app: configuração de exibição de versos na tela de configurações

--- app/src/screens/SettingsScreen/index.tsx
+++ app/src/screens/SettingsScreen/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react'
+import { NavigationProp, useFocusEffect, useNavigation } from '@react-navigation/native'
+import React, { useCallback, useState } from 'react'
+import CustomBibleVersePreview from '../../components/Bible/CustomBibleVersePreview'
 import SwitchLabel from '../../components/SwitchLabel'
 import { useSettings } from '../../hooks/settings'
-import { Container } from './styles'
+import { BibleVerseDisplaySettings } from '../../models/bible-verse-display-settings'
+import { AppStackParamList } from '../../routes/app.routes'
+import Storage from '../../utils/storage'
+import { PaddingHorizontalContainer, Scroll } from './styles'
 
 const SettingsScreen: React.FC = () => {
   const { theme, setTheme } = useSettings()
 
+  const navigation = useNavigation<NavigationProp<AppStackParamList>>()
+
+  const [bibleVerseDisplaySettings, setBibleVerseDisplaySettings] = useState<BibleVerseDisplaySettings>({})
+
+  useFocusEffect(useCallback(() => {
+    Storage.get('bible_verse_display_settings').then(displaySettings => {
+      setBibleVerseDisplaySettings(displaySettings || {})
+    })
+  }, []))
+
+  function handleCustomBibleVersePreviewPress() {
+    navigation.navigate('BibleVerseDisplaySettings')
+  }
+
   return (
-    <Container>
-      <SwitchLabel
-        label="Tema escuro"
-        value={theme == 'dark'}
-        onChange={isDark => setTheme(isDark ? 'dark' : 'light')}
+    <Scroll>
+      <PaddingHorizontalContainer>
+        <SwitchLabel
+          label="Tema escuro"
+          value={theme == 'dark'}
+          onChange={isDark => setTheme(isDark ? 'dark' : 'light')}
+        />
+      </PaddingHorizontalContainer>
+      <CustomBibleVersePreview
+        bibleVerse={{
+          text: 'Este é um texto fictício para testar a visualização customizada de versículos bíblicos.',
+          reference: {
+            bookName: 'Livro',
+            chapterNumber: 0,
+            verseNumber: 0
+          }
+        }}
+        onPress={handleCustomBibleVersePreviewPress}
+        displaySettings={bibleVerseDisplaySettings}
       />
-    </Container>
+    </Scroll>
   )
 }
 
--- app/src/screens/SettingsScreen/styles.ts
+++ app/src/screens/SettingsScreen/styles.ts
@@ -1,6 +1,10 @@
-import { ScrollView } from "react-native"
+import { ScrollView, View } from "react-native"
 import { styled } from "../../hooks/theme"
 
-export const Container = styled(ScrollView)`
-    padding: 16px 24px;
+export const Scroll = styled(ScrollView)`
+    padding: 16px 0px;
+`
+
+export const PaddingHorizontalContainer = styled(View)`
+    padding: 0px 16px;
 `
